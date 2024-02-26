630f4c1228cb1f064296f6dbedbb6c33cbf74077
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-26 11:45:36 +0530

    feat:adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkPrimary : "#314247",
+        darkSecondary: "#647973",
+        lightPrimary:"#CCA4A6",
+        lightSecondary:"#F7F7F7"
+      }
+    },
   },
   plugins: [],
 }
