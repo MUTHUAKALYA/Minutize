d2ff1c16500f42e0116eedb5a784f58d915039ae
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-26 11:50:10 +0530

    feat: adding routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import Header from './Components/Header.jsx'
+
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+// import General from './layout/General.jsx'
+// import Homepage from "./pages/Homepage.jsx"
+// import About from "./pages/About.jsx"
+// import Blogs from "./pages/Blogs.jsx"
+// import Travel from './pages/Travel.jsx'
+// import Food from "./pages/Food.jsx"
+// import Fitness from './pages/Fitness.jsx'
+
+
+
+// const router = createBrowserRouter([{
+//   path:"/",
+//   element:<General/>,
+//   children:[
+//     {
+//       path:"/",
+//       element:<Homepage/>
+//   },
+//   {
+//     path:"/about",
+//     element:<About/>
+//   },
+//   {
+//     path:"/blogs",
+//     element:<Blogs/>
+//   },
+//   {
+//     path:"/Travel",
+//     element:<Travel/>
+//   },
+//   {
+//     path:"/Food",
+//     element:<Food/>
+//   },
+//   {
+//     path:"/Fitness",
+//     element:<Fitness/>
+//   }
+// ]
+// }])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  // <RouterProvider router={router}/>
+  <Header/>
+ 
 )
